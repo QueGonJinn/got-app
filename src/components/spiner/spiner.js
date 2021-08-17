5f436c8308dc41a0aa7543774ccd5fdd886d4355
QueGonJinn <molialow13@gmail.com>
2021-08-17 12:18:28 +0300

    create app

---
+++ src/components/spiner/spiner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './spiner.css';
+
+const Spiner = () => {
+    return (
+        <div class="loadingio-spinner-rolling-li9kl885m9"><div class="ldio-uq295siqdg">
+        <div></div>
+        </div></div>
+    )
+}
+
+export default Spiner;
+
