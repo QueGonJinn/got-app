f436e58dbe596fea8b6fdd60ac102869e2b15b30
QueGonJinn <molialow13@gmail.com>
2021-08-18 11:12:03 +0300

    implementation of the button for switching the visibility of a random character

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
+import ErrorMessage from '../errorMessage';
 
 
-const App = () => {
-    return (
-        <> 
+export default class App extends Component {
+
+    state = {
+        showRandomChar: true,
+        error: false
+    }
+
+    toggleRandomChar = () => {
+        this.setState((state) => {
+            return {
+                showRandomChar: !state.showRandomChar
+            }
+        });
+    }
+
+
+    render() {
+
+        if (this.state.error) {
+            return <ErrorMessage/>
+        }
+
+        const char = this.state.showRandomChar ? <RandomChar/> : null;
+        return(
+            <> 
             <Container>
                 <Header />
             </Container>
             <Container>
                 <Row>
                     <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
+                        {char}
+                        <button 
+                            className="toogle__random"
+                            onClick={this.toggleRandomChar}>Toggle random character</button>
                     </Col>
                 </Row>
                 <Row>
@@ -28,7 +54,6 @@ const App = () => {
                 </Row>
             </Container>
         </>
-    );
-};
-
-export default App;
+        )
+    }
+}
